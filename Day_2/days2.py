84067af4113195f8dfb6d5cf9aa46bc83bfb9e8b
Tejasbisen786 <bisentejas@gmail.com>
2023-08-11 08:01:11 +0530

    Day-02 Added

---
+++ Day_2/days2.py
@@ -0,0 +1,39 @@
+
+# **Dynamic Typing
+username="Tejas"
+print(username)
+
+username=False
+print(username)
+
+# ** Name Convension 
+#  snake_case: get_all_folder
+#  pascal case : Getall
+# camel case  : getAllFolder
+
+
+# *Not Allowed 
+# 1tejas
+# @tushr
+# *if 
+
+mobile_Primary_ : 9834339517
+mobile_Secondary_: 9168383674
+
+#Range Function 
+# for i in range(10):
+    # print(i)
+     
+
+    #  mutable  :  changes
+    #  immutable : do not changes
+
+    #  list : ordered | repeated | chnaged
+    #  tuple : orderd | repeated | unchanged
+    # set : unorderd | chnageable | repeatble
+
+
+value = None  # Null 
+
+    # sum= 0 insted of that sum=None
+print(type(value))
