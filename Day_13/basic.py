b4c789797cb867d740bcf0a5cc8c724ac0cfc644
Tejasbisen786 <bisentejas@gmail.com>
2023-08-21 07:59:53 +0530

    TEst Conducted

---
+++ Day_13/basic.py
@@ -0,0 +1,9 @@
+# def add(x,y):
+#     return x+y
+
+
+add=lambda x,y:x+y
+print(add(12,34))
+
+
+# Test Conducted 
--- Day_6/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-set3.add(4)
-# set3.update([3,4,5,6])
-# set3.remove(3)
-# set3.discard('Apple')
