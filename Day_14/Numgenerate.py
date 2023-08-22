a714ef43f53cbb45b7a5313d579dc4d715630c51
Tejasbisen786 <bisentejas@gmail.com>
2023-08-22 07:45:51 +0530

    Python KBC GAME

---
+++ Day_14/Numgenerate.py
@@ -0,0 +1,6 @@
+import random
+
+options = ['Nagpur', 'Gondia', 'Chandrapur', 'Bhandara']  # Your list of options
+selected_option = random.choice(options)
+correct_option="Amravati"
+print("Selected option:", selected_option , correct_option)
